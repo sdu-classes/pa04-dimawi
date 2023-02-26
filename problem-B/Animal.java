db88c6606487509a044f0193a3de0b7d20307043
dimawi <124430243+dimawi@users.noreply.github.com>
2023-02-26 09:24:08 +0600

    Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+    int legs;
+
+    public Animal(int legs) {
+        this.legs = legs;
+    }
+    public abstract void eat();
+    public abstract void walk();
+}
+// осылай жасасақ кейін оверрайд арқылы өзгертеміз интерфестің функцияларын былай айтқанда
